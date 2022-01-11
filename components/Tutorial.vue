5c927e26cd2b681902b936902a0d3d86b9bc1eb2
MehedeeAlamAnon <mehedeealam@gmail.com>
2022-01-11 15:29:16 +0600

    second commit

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -6,7 +6,7 @@
       
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
-          Welcome to your Shanta Equity Application
+          Welcome to Shanta Equity Application
         </h2>
         
       </div>
